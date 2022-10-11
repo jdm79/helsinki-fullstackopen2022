24e0b70fa2578c3e0e1038d68694da084ec1da33
jdm79 <james.malvern@gmail.com>
2022-10-11 15:57:05 +0100

    Add Hello component which uses destructurisation

--- part1/src/App.js
+++ part1/src/App.js
@@ -28,6 +28,23 @@ const Part = (props) => {
   )
 }
 
+const Hello = (props) => {
+
+  const name = props.name
+  const age = props.age
+  const bornYear = () => new Date().getFullYear() - age
+  
+  
+  return (
+    <div>
+      <p>
+        Hello {name}, you are {age} years old
+      </p>
+      <p>So you were probably born in {bornYear()}</p>    
+    </div>
+  )
+}
+
 
 const App = () => {
 
@@ -51,6 +68,7 @@ const App = () => {
   
   return (
     <div>
+      <Hello name="James" age={43}/>
       <Header course={course.name} />
       <Content parts={course.parts} />
       <Total parts={course.parts} />
