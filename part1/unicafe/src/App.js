f6a1fe999f3115c711baf9a59228f20850f6b302
jdm79 <james.malvern@gmail.com>
2022-10-14 19:44:56 +0100

    Add a running total

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,41 +1,52 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [goodValue, setGoodValue] = useState(0)
-  const [badValue, setBadValue] = useState(0)
-  const [neutralValue, setNeutralValue] = useState(0)
+  const [good, setGood] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [total, setTotal ] = useState(0)
+
   
-  const setGood = () => {    
-    setGoodValue(goodValue + 1)  
+  const setGoodValue = () => {    
+    setGood(good + 1)
+    setTotalValue()
   }  
   
-  const setBad = () => {    
-    setBadValue(badValue + 1)  
+  const setBadValue = () => {    
+    setBad(bad + 1)
+    setTotalValue()
   }  
   
-  const setNeutral = () => {    
-    setNeutralValue(neutralValue + 1)  
+  const setNeutralValue = () => {    
+    setNeutral(neutral + 1)
+    setTotalValue()
   }  
 
   const resetValues = () => {    
-    setGoodValue(0)  
-    setBadValue(0)  
-    setNeutralValue(0)  
-  }  
+    setGood(0)  
+    setBad(0)  
+    setNeutral(0)
+    setTotal(0)
+  }
+
+  const setTotalValue = () => {
+    setTotal(total + 1)
+  }
+
     
-  
 
   return (
     <div>
       <h1>Give feedback</h1>
-      <button onClick={setGood}>Good</button>      
-      <button onClick={setBad}>Bad</button>
-      <button onClick={setNeutral}>Neutral</button>     
+      <button onClick={setGoodValue}>Good</button>      
+      <button onClick={setBadValue}>Bad</button>
+      <button onClick={setNeutralValue}>Neutral</button>     
 
       <h2>Statistics</h2>
-      <p>Good {goodValue}</p>
-      <p>Bad {badValue}</p>
-      <p>Neutral {neutralValue}</p>
+      <p>Good {good}</p>
+      <p>Bad {bad}</p>
+      <p>Neutral {neutral}</p>
+      <p>Total {total}</p>
       <button onClick={resetValues}>Reset</button>     
 
     </div>
