8ab0646c94cd4dc48c8fcecdd12f6445621423ef
jdm79 <james.malvern@gmail.com>
2022-10-25 19:04:10 +0100

    Make dynamic list using state and random and using spread array

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,67 +1,30 @@
-import {useState} from "react"
+import { useState } from 'react'
 
-const Button = (props) => {
-  return (
-    <button onClick={props.onclick}>{props.title}</button>
-  )
-}
 
 const App = () => {
-  const anecdotes = [
-    "If it hurts, do it more often",
-    "Adding manpower to a late software project makes it later!",
-    "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
-    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
-    "Premature optimization is the root of all evil.",
-    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
-    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients",
-  ]
+  const [nums, setNums] = useState([]) 
 
-  const [selected, setSelected] = useState(0)
-  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0))
+  const array1 = [0,2,4,6,8]
+  const array2 = [1,3,5,7,9]
+  const mergedArray = [...array1, ...array2]
 
-  const getRandomInt = max => Math.floor(Math.random() * max)
-  const randomAnecdote = () => setSelected(getRandomInt(anecdotes.length))
-  const voteAnecdote = () => {
-    const votesCopy = [...votes]
-    votesCopy[selected] += 1
-    setVotes(votesCopy)
-  }
+  const showArray = () => {
+    const copy = []
+    let random = Math.random()
+    mergedArray.sort().forEach(num => copy.push(num * random))
 
-  const mostVotes = () => {
-    let max = Math.max(...votes)
-    return votes.indexOf(max)
-  }
+    setNums(copy);
+
+  };
 
   return (
-    <div className="container">
-      <h1>Anecdote of the day</h1>
-      <div className="anecdote">
-        <p>{anecdotes[selected]}</p>
-        <p>has {votes[selected]} votes</p>
-      </div>
-      <div className="buttons">
-        <Button onclick={voteAnecdote} title="vote"/>
-        <Button onclick={randomAnecdote} title="next anecdote"/>
-      </div>
-      {votes[mostVotes()] === 0 ? (
-        <div className="most-votes">
-          <h1>Anecdote with most votes</h1>
-          <div className="anecdote">
-            <p>Let's get voting! 0 votes so far...</p>
-          </div>
-        </div>
-      ) : (
-        <div className="most-votes">
-          <h1>Anecdote with most votes</h1>
-          <div className="anecdote">
-            <p>{anecdotes[mostVotes()]}</p>
-            <p>has {votes[mostVotes()]} votes</p>
-          </div>
-        </div>
-      )}
+    <div>
+      <button onClick={showArray}>Click</button>
+      <h1>Numbers!</h1>
+      <ul>{nums.map(num => <li>{num}</li>)}</ul>
     </div>
-  );
+
+  )
 }
 
 export default App
